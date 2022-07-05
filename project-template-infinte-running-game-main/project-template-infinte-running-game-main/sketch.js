32cdaf1222d5c8c4d004faff9f4aa4cf9ac3d1cf
CounterKerry <102113347+CounterKerry@users.noreply.github.com>
2022-07-05 17:26:49 -0300

    Add files via upload

---
+++ project-template-infinte-running-game-main/project-template-infinte-running-game-main/sketch.js
@@ -0,0 +1,23 @@
+var gamesEduuu, gamesEduuu_image;
+var edinaldoPereira, edinaldoPereira_image;
+var lula, lulaImg;
+var bolsonaro, bolsonaroImg;
+var estrada, estradaImg;
+
+function preload() {
+    estrada = loadImage("estradaImagem.jpn")
+    bolsonaroImg = loadImage("Bolsonaro-removebg-preview.png");
+    lulaImg = loadImage();
+    edinaldoPereira_image = loadImage();
+    gamesEduuu_image = loadImage();
+}
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+
+    estrada = createSprite(width/2, height/4);
+}
+
+function draw() {
+ 
+}
